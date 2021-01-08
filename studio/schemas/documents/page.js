7f06ba55ddf4093b2334d597bd28fdd32fdb36bb
Scott Taylor <unrealtension1996@gmail.com>
2021-01-08 12:48:30 +0000

    Removed SEO Tools as it had really outdated packages

--- studio/schemas/documents/page.js
+++ studio/schemas/documents/page.js
@@ -42,33 +42,6 @@ export default {
         collapsed: false,
       }
     },
-    {
-      name: 'seo',
-      title: 'SEO (Powered by Yoast)',
-      type: 'seo-tools',
-      options: {
-        collapsible: true,
-        collapsed: true,
-        baseUrl: url,
-        slug(doc) {
-          return doc.slug.current;
-        },
-        fetchRemote: true,
-        content(doc) {
-          return 'simple html representation of your doc'; // (OPTIONAL) If your site is generated after Sanity content updates you can use this for better real time feedback
-        },
-        title(doc) {
-          return 'page title'; // (OPTIONAL) return page title otherwise inferred from scrape
-        },
-        description(doc) {
-          return 'page description'; // (OPTIONAL) return page description otherwise inferred from scrape
-        },
-        locale(doc) {
-          return 'page locale'; // (OPTIONAL) return page locale otherwise inferred from scrape
-        },
-        contentSelector: 'body' // (OPTIONAL) option to finetune where Yoast will look for the content. (only applicable for scraping without content function)
-      },
-    }
   ],
   preview: {
     select: {
