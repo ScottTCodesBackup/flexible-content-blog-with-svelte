8abd0d835f175bcacf75f803878b6287019ced16
Scott Taylor <scott1996taylor@outlook.com>
2021-01-11 15:25:57 +0000

    Updated index page

--- web/src/routes/blog/index.svelte
+++ web/src/routes/blog/index.svelte
@@ -30,10 +30,6 @@
 
 <ul>
 	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug.current}'>{post.title}</a> ({formatDate(post.publishedAt)})</li>
+		<li><a rel="prefetch" href="blog/{post.slug.current}">{post.title}</a> ({formatDate(post.publishedAt)})</li>
 	{/each}
 </ul>
